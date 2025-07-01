dfed5712cc0c5bdd47dbc713e07f670eb84b45e0
robinnn20 <113219415+robinnn20@users.noreply.github.com>
2025-06-30 20:02:35 -0400

    Update db.js

--- bus_service_helpers/db.js
+++ bus_service_helpers/db.js
@@ -1,6 +1,6 @@
 // bus_service_helpers/db.js
 
-const  Pool  = require('pg');
+const  {Pool}  = require('pg');
 const path = require('path');
 
 // Cargamos las variables de entorno globales del proyecto  
